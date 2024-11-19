940cdb862c0229d6b7afea85949f8e3237572d6d
mahmudul01 <69353551+mahmudul01@users.noreply.github.com>
2024-11-19 11:27:05 +0600

    Theme replaced

---
+++ src/VmsTheme.Web/Account/Settings/AccountSettingNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VmsTheme.Web.Account.Settings;
+public class AccountSettingNames
+{
+    public const string IsSelfRegistrationEnabled = "Abp.Account.IsSelfRegistrationEnabled";
+
+    public const string EnableLocalLogin = "Abp.Account.EnableLocalLogin";
+}
