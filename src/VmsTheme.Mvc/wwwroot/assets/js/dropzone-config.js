f564c6b5337982fd7bee6566e4a5dfa7b39d6478
jinhridoy <jinhridoy@gmail.com>
2024-11-24 15:04:20 +0600

    no message

---
+++ src/VmsTheme.Mvc/wwwroot/assets/js/dropzone-config.js
@@ -0,0 +1,16 @@
+﻿function initializeDropzone(selector)
+{
+    var dropzoneOptions =
+    {
+        url: "api/file", // Your API endpoint
+        paramName: "file",
+        maxFilesize: 5, // Max file size in MB
+        acceptedFiles: ".jpg,.jpeg,.png,.pdf", // Allowed file types
+        addRemoveLinks: true,
+        success: function (file, response) {
+            debugger;
+        }
+    };
+
+    new Dropzone(selector, dropzoneOptions);
+}
