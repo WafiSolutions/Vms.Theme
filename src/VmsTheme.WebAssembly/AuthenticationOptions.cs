add3c9b48768c72f937624203026e2d71d795339
jinhridoy <jinhridoy@gmail.com>
2024-11-24 13:31:22 +0600

    no message

--- src/VmsTheme.WebAssembly/AuthenticationOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace VmsTheme.WebAssembly;
-
-public class AuthenticationOptions
-{
-    public string LoginUrl { get; set; } = "authentication/login";
-
-    public string LogoutUrl { get; set; } = "authentication/logout";
-}
